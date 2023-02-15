73991c59ccd146576e7a2d68cda9f260e47e09fc
qingyun <rivendinner@gmail.com>
2023-02-16 00:05:33 +0800

    basic architecture

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigName("config.yaml")
+	viper.AddConfigPath("./config")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	server := gin.Default()
+
+	server.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"health": "ok",
+		})
+	})
+
+	server.Run(":8080")
 }
